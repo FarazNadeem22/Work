e28d868320d0a3f7e5c560078b52dda8c55ab7d7
Faraz Nadeem <66399838+FarazNadeem22@users.noreply.github.com>
2023-12-13 19:45:19 -0500

    Update emailScriptMultiple.py

--- emailScriptMultiple.py
+++ emailScriptMultiple.py
@@ -1,6 +1,5 @@
 import socket
 from datetime import datetime
-import time
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -16,29 +15,29 @@ def get_ip():
 
 def send_email(ip_address):
     # Setting up variables
-    from_email = 'Your email'
-    recipients = ['first recipients email', 'second recipients email', 'third recipients email']
-    smtp_server = 'smtp Server'
+    from_email = 'maf22@gmail.com'  # Replace 'Your email' with your email address
+    recipients = ['recipient1@example.com', 'recipient2@example.com']  # Add recipient email addresses
+    smtp_server = '198.233.3.180'  # Replace 'smtp Server' with your SMTP server IP address or domain
     current_time = datetime.now().strftime("%H:%M:%S")
     smtp_port = 25
 
     # Construct email
     message = MIMEMultipart()
     message_text = f"This is a test\n The current ip address is: {ip_address}\nLocal time: {current_time}"
-    message["subject"] =  f"The current ip address is: {ip_address}"
-    message["FROM"] = from_email
-    message["To"] = ",".join(recipients)
+    message["Subject"] = f"The current ip address is: {ip_address}"
+    message["From"] = from_email
+    message["To"] = ", ".join(recipients)
     message.attach(MIMEText(message_text))
 
     # Connecting to Server
     server = smtplib.SMTP(smtp_server, smtp_port)
     try:
         # Send email and quit()
-        server.sendmail(from_email,recipients, message.as_string())
+        server.sendmail(from_email, recipients, message.as_string())
         print("Email sent")
         server.quit()
-    except:
-        print("Something went wrong")
+    except Exception as e:
+        print("Something went wrong:", e)
         server.quit()
 
 
