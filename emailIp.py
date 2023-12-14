ff6964b771c6cc5b8830a75facb6f3d44580b745
Faraz Nadeem <66399838+FarazNadeem22@users.noreply.github.com>
2023-12-13 19:50:03 -0500

    Update emailIp.py

--- emailIp.py
+++ emailIp.py
@@ -3,38 +3,38 @@ import schedule
 import time
 import smtplib
 
-
 def get_ip():
-
     # Get hostname and ip address
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
     
-    # Send email
+    # Send email with obtained IP address
     send_email(ip_address)
     
 def send_email(ip_address):
+    # Setting up email variables
+    from_email = 'sender@example.com'  # Your email address
+    to_email = 'recipient@example.com'  # Recipient's email address
+    my_password = 'password'  # Password for the sender's email account
+    smtp_server = 'smtp.example.com'  # SMTP server details (e.g., 'smtp.gmail.com' for Gmail)
+    smtp_port = 587  # SMTP port number (587 for TLS, 465 for SSL)
+
+    # Connecting to SMTP server
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()  # Initiating TLS connection
+    server.login(from_email, my_password)  # Logging into the sender's email account
     
-    # Setting up variables
-    from_email = ''
-    to_email = ''
-    my_password = 'passworD'
-    smtp_server = ''
-
-    # Connecting to Server
-    server = smtplib.SMTP(smtp_server, 507)
-    server.starttls()
-    server.login(from_email, my_password)
-
-    # Create message 
+    # Composing email message
     message = f"The IP address for the Acta Verba Machine is {ip_address}"
-
-    # Send email and quit()
-    server.sendmail(from_email,to_email, message)
+    
+    # Sending email and quitting the SMTP server connection
+    server.sendmail(from_email, to_email, message)
     server.quit()
 
+# Schedule to run 'get_ip' function every day at 09:00
 schedule.every().day.at("09:00").do(get_ip)
 
+# Infinite loop to continuously check and execute scheduled tasks
 while True:
-    schedule.run_pending()
-    time.sleep(1)
+    schedule.run_pending()  # Check if any scheduled tasks are pending
+    time.sleep(1)  # Sleep to avoid excessive CPU usage during the loop
